574dc7b17dad215d4c8b1ca3c9e215969c593b1c
Ralf342 <j.oskar.v282143@gmail.com>
2023-12-29 21:22:29 -0600

    Final

--- src/Principal/Principal.jsx
+++ src/Principal/Principal.jsx
@@ -46,19 +46,29 @@ function Principal(params) {
                 <div className="hero-section">
                     <img src="./Imagenes/headerPrincipal.webp" alt="Imagen header pagina principal" className="hero-image" />
                     <div className="hero-text">
-                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                        <h2>La esperanza fluye en cada donación. Juntos, superaremos esto</h2>
                     </div>
                 </div>
                 <div className="bottom-text">
                     <h2 >Bienvenido a Sangre Segura Xalapa</h2>
-                    <p className='centrarTextoPrincipal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, consequuntur aliquam deleniti maxime cumque voluptate fuga ducimus error consequatur consectetur dolorum itaque ipsam animi nisi delectus accusamus in voluptatem saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat facere ex harum dignissimos est a saepe suscipit aliquam earum! Error sunt culpa, perferendis nesciunt eligendi ipsum exercitationem adipisci porro inventore!</p>
+                    <p className='centrarTextoPrincipal'>Sangre Segura Xalapa ha sido un apoyo invaluable para mi familia. Gracias a ella pudimos llegar a mas personas  y conseguir la sangre necesaria para mi hija en un momento muy difícil.</p>
                 </div>
                 <div className="image-container">
                     <h2>Título del Contenedor</h2>
                     <div className="image-row">
                         <div className="image">
                             <img src="./Imagenes/retratoHombrePrincipal.webp" alt="Imagen Donante 1" />
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore odio soluta voluptatum sed recusandae quasi expedita eaque, ut est animi, enim voluptates temporibus. Obcaecati quaerat perferendis quo praesentium excepturi incidunt!</p>
+                            <p>Nombre: Pablo González
+
+                                Fecha de Nacimiento: 10 de abril de 1935
+
+                                Antecedentes Médicos:
+
+                                Hipertensión arterial diagnosticada en 1995.
+                                Diabetes tipo 2 diagnosticada en 2003.
+                                Osteoartritis crónica en las articulaciones de las rodillas desde 2010.
+                                Historial de eventos cardiovasculares en la familia.
+                            </p>
                         </div>
                         <div className="image">
                             <img src="./Imagenes/fachadaHospitalPrincipal.webp" alt="Imagen Donante 2" />
@@ -66,7 +76,12 @@ function Principal(params) {
                         </div>
                         <div className="image">
                             <img src="./Imagenes/retratoMujerPrincipal.webp" alt="Imagen Donante 3" />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus mollitia ipsam enim accusantium voluptatum molestiae perspiciatis? Dolores, sit? Possimus blanditiis suscipit excepturi quos veritatis illo sequi omnis ea nostrum quisquam.</p>
+                            <p>Nombre Ana Rodríguez
+                                Fecha de Nacimiento: 25 de julio de 1990
+                                Antecedentes Médicos:
+                                No se reportan antecedentes médicos significativos.
+                                No hay historial de enfermedades crónicas en la familia.
+                            </p>
                         </div>
                     </div>
                 </div>
